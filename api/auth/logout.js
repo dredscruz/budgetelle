2a1d71455d37ff8fce0de1fd45693249d6186799
dredscruz <dredscruz@gmail.com>
2026-08-23 17:26:29 +0400

    Fix require paths in auth functions

--- api/auth/logout.js
+++ api/auth/logout.js
@@ -1,5 +1,5 @@
 // Signs out of the Google session
-const { cookieHeader } = require('./_lib/session');
+const { cookieHeader } = require('../_lib/session');
 module.exports = (req, res) => {
   res.setHeader('Set-Cookie', cookieHeader('bt_session', '', 0));
   res.status(200).json({ ok: true });
--- api/auth/me.js
+++ api/auth/me.js
@@ -1,5 +1,5 @@
 // Returns the current Google session (if any)
-const { verify, parseCookies } = require('./_lib/session');
+const { verify, parseCookies } = require('../_lib/session');
 module.exports = (req, res) => {
   const s = verify(parseCookies(req).bt_session);
   if (!s) return res.status(200).json({ authenticated: false });
